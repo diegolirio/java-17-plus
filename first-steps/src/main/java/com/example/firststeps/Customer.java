ba6210478348239ad92c0c2ae9c033dd65dccde7
Diego Lirio <ddamacena@uolinc.com>
2022-12-16 18:20:59 -0300

    +

---
+++ first-steps/src/main/java/com/example/firststeps/Customer.java
@@ -0,0 +1,3 @@
+package com.example.firststeps;
+
+public record Customer(String firstname, Integer age) { }
---
+++ first-steps/src/main/java/com/example/firststeps/Java17Controller.java
@@ -0,0 +1,19 @@
+package com.example.firststeps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/customers")
+public class Java17Controller {
+
+    @GetMapping
+    public List<Customer> getList() {
+        return List.of(
+                new Customer("Diego", 35)
+        );
+    }
+}
